b43eb1acc087680403ea558724ca03be39669f9e
Nate Moore <nate@skypack.dev>
2022-03-26 06:04:21 -0500

    chore(test): remove outdated test

--- packages/astro/test/0-css.test.js
+++ packages/astro/test/0-css.test.js
@@ -291,11 +291,6 @@ describe('CSS', function () {
 			expect((await fixture.fetch(href)).status).to.equal(200);
 		});
 
-		it('resolves Astro styles', async () => {
-			const style = $('style[astro-style]');
-			expect(style.length).to.not.equal(0);
-		});
-
 		it('resolves Styles from React', async () => {
 			const styles = [
 				'ReactCSS.css',
